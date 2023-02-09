0e24798a967d75b25e1efa0a435ec69ec763c60c
fraanciiss04 <francisco.puras.alu@santamaria-artesano.org>
2023-02-09 09:44:15 +0100

    Version María 2

--- src/main/java/holamundo/HolaMundo.java
+++ src/main/java/holamundo/HolaMundo.java
@@ -12,6 +12,5 @@ public class HolaMundo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Soy María y estoy modificandolo en el remoto local");
     }
 }
