50ee63133051fd438e4e68b03b68f8ed26bcb78d
fraanciiss04 <francisco.puras.alu@santamaria-artesano.org>
2023-02-09 09:29:34 +0100

    Version María

---
+++ src/main/java/holamundo/MundoCruel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package holamundo;
+
+/**
+ *
+ * @author María y José
+ */
+public class MundoCruel {
+    public static void main(String[] args) {
+        System.out.println("Este mundo es cruel");
+        System.out.println("Pero solo con María");
+    }
+}
